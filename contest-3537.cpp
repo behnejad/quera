9da8003417bc030d1c7aa7545fa7b3ddd22867f0
hooman behnejad <hoomanbehnejad@gmail.com>
2020-03-13 12:44:23 +0330

    problem(s) solved

---
+++ contest-3537.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "W";
+    while (n--)
+    {
+        cout << "o";
+    }
+    cout << "w!" << endl;
+    return 0;
+}
