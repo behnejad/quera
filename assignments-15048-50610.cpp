8e5ac658dfb6212689c1af3c25406d7714136288
hooman behnejad <hoomanbehnejad@gmail.com>
2020-03-18 09:27:36 +0330

    problem(s) solved

--- assignments-15048-50610.cpp
+++ assignments-15048-50610.cpp
@@ -4,12 +4,60 @@
 
 using namespace std;
 
+vector<int> a;
+vector<int> scores;
+int total_min = numeric_limits<int>::max();
+
+void find(int index, int n, int i, int k)
+{
+    if (i == k)
+    {
+        int all_min = numeric_limits<int>::max();
+        int c = 0;
+        for (int j = 0; j < k; ++j)
+        {
+            int set_max = numeric_limits<int>::min();
+            for (int m = 0; m < scores[j]; ++m)
+            {
+                if (set_max < a[c])
+                {
+                    set_max = a[c];
+                }
+
+                ++c;
+            }
+
+            if (all_min > set_max)
+            {
+                all_min = set_max;
+            }
+        }
+
+        if (all_min < total_min)
+        {
+            total_min = all_min;
+        }
+    }
+    else if (i == k - 1)
+    {
+        scores[i] = n - index;
+        find(0, n, i + 1, k);
+    }
+    else
+    {
+        for (int j = 0; j < n - index + k - i - 3; ++j)
+        {
+            scores[i] = j + 1;
+            find(j + 1 + index, n, i + 1, k);
+        }
+    }
+}
+
 int main()
 {
     int n, k;
     int max = numeric_limits<int>::min();
     int min = numeric_limits<int>::max();
-    vector<int> a;
 
     cin >> n >> k;
     for (int i = 0; i < n; ++i)
@@ -38,7 +86,9 @@ int main()
     }
     else
     {
-
+        scores.resize(k);
+        find(0, n, 0, k);
+        cout << total_min << endl;
     }
 
     return 0;
