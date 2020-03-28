1d12d9ff3e1e174e8b2dbb7f7d92d536abfc7670
hooman behnejad <hoomanbehnejad@gmail.com>
2020-03-29 00:44:02 +0430

    more problem(s) solved

---
+++ contest-10234.cpp
@@ -0,0 +1,45 @@
+# include <iostream>
+# include <cstring>
+
+using namespace std;
+
+const int oo = 999999999;
+int dp[11][10001] , a[11] , n , m;
+
+int mem(int pos , int need)
+{
+    if (pos == 0)
+    {
+        return (need == 0) ? 0 : oo;
+    }
+
+    int &ret = dp[pos][need];
+
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    ret = oo;
+
+    for (int i = 0 ; i * i <= need ; i++)
+    {
+        ret = min(ret , mem(pos - 1 , need - i * i) + (a[pos] - i) * (a[pos] - i));
+    }
+
+    return ret;
+}
+
+int main()
+{
+    memset(dp , -1 , sizeof dp);
+    cin >> n >> m;
+    for (int i = 1 ; i <= n ; i++)
+    {
+        cin >> a[i];
+    }
+    int ans = oo;
+    ans = min(ans , mem(n , m));
+    cout << (ans == oo ? -1 : ans) << endl;
+    return 0;
+}
