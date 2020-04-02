f228e331624344fe0fe4935e54ff012a4c8b532e
hooman behnejad <hoomanbehnejad@gmail.com>
2020-04-02 20:14:28 +0430

    more problem(s) solved

---
+++ contest-18311.cpp
@@ -0,0 +1,61 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+char times[24 * 60 * 60];
+char correct[15 * 60];
+
+int main()
+{
+    memset(times, 1, sizeof times);
+    memset(correct, 1, sizeof correct);
+    int i, start, end, h, m, s;
+
+    scanf("%d", &i);
+    while (i--)
+    {
+        scanf("%d:%d:%d", &h, &m, &s);
+        start = h * 3600 + m * 60 + s;
+        scanf("%d:%d:%d", &h, &m, &s);
+        end = h * 3600 + m * 60 + s;
+        memset(times + start, 0, end - start + 1);
+    }
+
+    scanf("%d", &i);
+    while (i--)
+    {
+        scanf("%d:%d:%d", &h, &m, &s);
+        start = h * 3600 + m * 60 + s;
+        scanf("%d:%d:%d", &h, &m, &s);
+        end = h * 3600 + m * 60 + s;
+        memset(times + start, 0, end - start + 1);
+    }
+
+    scanf("%d", &i);
+    while (i--)
+    {
+        scanf("%d:%d:%d", &h, &m, &s);
+        start = h * 3600 + m * 60 + s;
+        scanf("%d:%d:%d", &h, &m, &s);
+        end = h * 3600 + m * 60 + s;
+        memset(times + start, 0, end - start + 1);
+    }
+
+    bool found = false;
+    for (int i = 0; i < sizeof(times) - 15 * 60; ++i)
+    {
+        if (memcmp(times + i, correct, 15 * 60) == 0)
+        {
+            found = true;
+            printf("%02d:%02d:%02d\n", i / 3600, (i % 3600) / 60, i % 60);
+            break;
+        }
+    }
+
+    if (found == false)
+    {
+        printf("-1\n");
+    }
+}
