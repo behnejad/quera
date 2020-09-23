19f922f49d9290ab0defa0d9891c19612368bfa9
Hooman Behnejad <h.behnejad@sinacomsys.com>
2020-09-23 16:31:04 +0330

    :)

--- university-294.cpp
+++ university-294.cpp
@@ -1,67 +1,52 @@
 #include <iostream>
 #include <iomanip>
-#include <limits>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-    double ep = numeric_limits<double>::epsilon();
-    double a, b, c;
+    long double a, b, c;
     cin >> a >> b >> c;
-    double x, y;
+    long double x, y;
 
-    if (fabs(a) < ep && fabs(b) < ep)
+    cout << fixed << setprecision(3);
+
+    if (a == 0 && b == 0)
     {
-        cout << "IMPOSSBILE" << endl;
+        cout << "IMPOSSIBLE" << endl;
     }
-    else if (fabs(c) < ep && (fabs(a) < ep || fabs(b) < ep))
+    else if ((a == 0 && c == 0) || (b == 0 && c == 0))
     {
         cout << "0.000" << endl;
     }
     else
     {
-        if (fabs(a) < ep)
+        if (a == 0)
         {
             x = (-c) / b;
-            cout << fixed << setprecision(3) << x << endl;
+            cout << x << endl;
         }
         else
         {
-            double qrt = (b * b) - (a * c * 4);
-            if (qrt < 0.0)
+            long double qrt = (b * b) - ((a * c) * 4);
+            if (qrt < 0)
             {
                 cout << "IMPOSSIBLE" << endl;
             }
-            else if (fabs(qrt) < ep)
+            else if (qrt == 0)
             {
-                x = ((-b) / a) / 2;
-                cout << fixed << setprecision(3) << x << endl;
-            }
-            else if (fabs(sqrt(qrt)) < ep)
-            {
-                x = ((-b) / a) / 2;
-                cout << fixed << setprecision(3) << x << endl;
+                x = (-b) / (a * 2);
+                cout << x << endl;
             }
             else
             {
-                x = ((-b) - sqrt(qrt)) / (a * 2);
-                y = ((-b) + sqrt(qrt)) / (a * 2);
-                if (x < y)
-                {
-                    cout << fixed << setprecision(3) << x << endl
-                         << fixed << setprecision(3) << y << endl;
-                }
-                else
-                {
-                    cout << fixed << setprecision(3) << y << endl
-                         << fixed << setprecision(3) << x << endl;
-                }
+                x = ((-b) - sqrtl(qrt)) / (a * 2);
+                y = ((-b) + sqrtl(qrt)) / (a * 2);
+                cout << x << endl << y << endl;
             }
         }
     }
-
 
     return 0;
 }
