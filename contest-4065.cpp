a5a21dec839e71238e474a702dc66f53e11fdba9
Hooman Behnejad <hoomanbehnejad@gmail.com>
2020-09-24 15:20:26 +0330

    :)

--- contest-4065.cpp
+++ contest-4065.cpp
@@ -4,18 +4,8 @@ using namespace std;
 
 int main()
 {
-    int a, b, l;
-    cin >> a >> b >> l;
-
-    int time = 0;
-    bool is_a = true;
-
-    while (l--)
-    {
-        time += (is_a) ? a : b;
-        is_a = !is_a;
-    }
-
-    cout << time << endl;
-    return 0;
+   int a,b,l;
+   cin >> a >> b >> l;
+   cout << (l % 2 == 0) ? ((a + b) * (l / 2)) : (((a + b) * (l / 2)) + a)<< endl;
+   return 0;
 }
