a79471a59b574d867801b409f4ddad7e23d6c201
Hooman Behnejad <hoomanbehnejad@gmail.com>
2020-09-24 16:10:30 +0330

    :)

--- contest-3541.py
+++ contest-3541.py
@@ -1,21 +1,4 @@
+import math
 n = int(input())
-sit = set()
-count = 0
-for i in range(1, n // 2 + 1):
-    ab = n - i
-    for j in range(ab // 2, ab - i + 1):
-        k = ab - j
-##        print(i, ab,  j, k)
-        if (i, j, k) in sit:
-            continue
-        
-        if i + j > k and i + k > j and j + k > i:
-            sit.add((i, j, k))
-            sit.add((i, k, j))
-            sit.add((j, i, k))
-            sit.add((j, k, i))
-            sit.add((k, i, j))
-            sit.add((k, j, i))
-            count += 1
 
-print(count)
+print(round(n ** 2 / 48)) if n % 2 == 0 else print(round((n + 3)** 2 / 48))
