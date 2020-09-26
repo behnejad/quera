91deb2c3ae908a5a4418f2fe6363161a3c93be03
Behnejad <3690943+behnejad@users.noreply.github.com>
2020-09-26 09:28:15 +0330

    :)

--- university-9742.py
+++ university-9742.py
@@ -1,34 +1,50 @@
-users = []
-di = {}
-find = 1
+users=dict()
+idsList=[]
+data=[]
+output=[]
+findCount=0
+userNotFoundRes="No user found"
+userFound = 0
+limit=0
+
+def find_user(count,searchKey):
+    for key in sorted(users):
+        if str(key).startswith(searchKey):
+           global limit
+           limit=limit+1
+           if limit<=10:
+            output.append(str(count)+" "+users.get(key))
+           global userFound
+           userFound=1
+
+
+
+def add_user(arr):
+ for i in range(0,len(arr)):
+     if len(arr[i].split(" ")) == 5:
+            id=arr[i].split(" ")[4]
+            idsList.append(id)
+            users[id]=arr[i].split(" ")[1]+" "+arr[i].split(" ")[2]+" "+arr[i].split(" ")[3]+" "+arr[i].split(" ")[4]
+            output.append("User "+id+" added successfully")
+     if len(arr[i].split(" ")) == 2:
+            key=arr[i].split(" ")[1]
+            global findCount
+            findCount =findCount+1
+            find_user(findCount,key)
+            global limit
+            limit=0
+            global userFound
+            if userFound!=1:
+                output.append(str(findCount)+" "+userNotFoundRes)
+     userFound=0
+
+
+
 try:
     while True:
-        line = input().split()
-        if line[0] == "Add":
-            di[line[4]] = line[1:-1]
-            users.append(line[4])
-            users = sorted(users)
-            print("User", line[4], "added successfully")
-        elif line[0] == "Find":
-            index = -1
-            if line[1] in di:
-                l = di[line[1]]
-                print(find, l[0], l[1], l[2], line[1])
-            else:
-                for i in range(len(users)):
-                    if users[i].startswith(line[1]):
-                        index = i
-                        break
-                if index == -1:
-                    print(find, "No user found")
-                else:
-                    for i in range(10):
-                        if users[index + i].startswith(line[1]):
-                            l = di[users[index + i]]
-                            print(find, l[0], l[1], l[2], users[index + i])
-                        else:
-                            break
-
-            find += 1
-except:
-    pass
+     data.append(input())
+except EOFError as error:
+    add_user(data)
+    idsList.sort()
+    for i in range(len(output)):
+        print(output[i])
