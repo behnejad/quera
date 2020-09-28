cbe94c184c00f861fa8130dadf793063d2181bce
Hooman Behnejad <h.behnejad@sinacomsys.com>
2020-09-28 13:56:16 +0330

    :)

--- university-288.py
+++ university-288.py
@@ -4,13 +4,38 @@ m = int(i[1])
 ma = [input().split() for i in range(n)]
 i, j, e = 0, 0, 0
 
+def do_map():
+    global ma, i, j, e
+    if ma[i][j].lower() == 'c':
+        print("Frog hunted by crocodile :-/! Energy %d!" % e)
+        exit(0)
+    elif ma[i][j][0].lower() == 'e':
+        d = int(ma[i][j][2:-1])
+        e += d
+        if e <= 0:
+            print("Frog died! Energy %d!" % e)
+            exit(0)
+        else:
+            print("Energy %d!" % e)
+    elif ma[i][j][0].lower() == 'j':
+        di, dj = ma[i][j][2:-1].split(',')
+        di = int(di)
+        dj = int(dj)
+        if di < 0 or di >= n or dj < 0 or dj >= m:
+            print("Impossible place!")
+        else:
+            print("Jump to (%d,%d)" % (di, dj))
+            i = di
+            j = dj
+            do_map()
+
 while True:
     try:
         c = input().lower()
         do = False
         if c == "finish":
             print("Energy %d!" % e)
-            break;
+            break
         elif c == "right":
             if j == m - 1:
                 print("Impossible place!")
@@ -37,33 +62,9 @@ while True:
                 do = True
         elif c == "location":
             print("Location (%d,%d)" % (i, j))
-
+        
         if do:
-            if ma[i][j].lower() == 'c':
-                print("Frog hunted by crocodile :-/! Energy %d!" % e)
-                break
-            elif ma[i][j][0].lower() == 'e':
-                d = int(ma[i][j][2:-1])
-                if d % 5 == 0:
-                    e += d
-                    if e <= 0:
-                        print("Frog died! Energy %d!" % e)
-                        break
-                    else:
-                        print("Energy %d!" % e)
-            elif ma[i][j][0].lower() == 'j':
-                org = ma[i][j]
-                di, dj = ma[i][j][2:-1].split(',')
-                di = int(di)
-                dj = int(dj)
-                if di < 0 or di >= n:
-                    print("Impossible place!")
-                elif dj < 0 or dj >= m:
-                    print("Impossible place!")
-                else:
-                    print("Jump to (%d,%d)" % (di, dj))
-                    i = di
-                    j = dj
+            do_map()
                     
     except:
         break
